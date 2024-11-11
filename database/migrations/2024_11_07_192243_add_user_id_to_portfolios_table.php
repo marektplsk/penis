890b3983c0d517bacf1f929861e2310bc62a7465
luqeey <115131091+luqeey@users.noreply.github.com>
2024-11-11 09:51:20 +0100

    portfolio table fix

--- database/migrations/2024_11_07_192243_add_user_id_to_portfolios_table.php
+++ database/migrations/2024_11_07_192243_add_user_id_to_portfolios_table.php
@@ -11,11 +11,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('portfolios', function (Blueprint $table) {
-            if (!Schema::hasColumn('portfolios', 'user_id')) {
-                $table->unsignedBigInteger('user_id')->after('type'); // Add user_id column
-                $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade'); // Foreign key relation to users table
-            }
+        Schema::create('portfolios', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('amount', 15, 2);
+            $table->string('type');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
         });
     }
 
@@ -24,9 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('portfolios', function (Blueprint $table) {
-            $table->dropForeign(['user_id']); // Drop foreign key
-            $table->dropColumn('user_id'); // Drop user_id column
-        });
+        Schema::dropIfExists('portfolios');
     }
 };
--- database/migrations/2024_11_08_171115_add_tags_to_wins_table.php
+++ database/migrations/2024_11_08_171115_add_tags_to_wins_table.php
@@ -4,19 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTagsTable extends Migration
+class AddTagsToWinsTable extends Migration
 {
-public function up()
-{
-Schema::create('tags', function (Blueprint $table) {
-$table->id();                        // Creates an auto-incrementing 'id' column as the primary key
-$table->string('name')->unique();     // Creates a 'name' column that must be unique
-$table->timestamps();                 // Creates 'created_at' and 'updated_at' timestamp columns
-});
-}
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();                        // Creates an auto-incrementing 'id' column as the primary key
+            $table->string('name')->unique();     // Creates a 'name' column that must be unique
+            $table->timestamps();                 // Creates 'created_at' and 'updated_at' timestamp columns
+        });
+    }
 
-public function down()
-{
-Schema::dropIfExists('tags');             // Drops the 'tags' table if it exists, rolling back the migration
-}
+    public function down()
+    {
+        Schema::dropIfExists('tags');             // Drops the 'tags' table if it exists, rolling back the migration
+    }
 }
